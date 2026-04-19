75958fa5fc34d3e2ab6a91ececec443c68532d03
Daniel Newton <djpnewton@gmail.com>
2026-04-19 11:48:53 -0400

    add tree fractal, add some sliders

--- src/main.zig
+++ src/main.zig
@@ -25,6 +25,56 @@ fn backBtn() bool {
     return rg.button(.init(24, 24, size, size), "<");
 }
 
+fn interationsSlider(iterations: *usize) void {
+    // iterations slider
+    const bounds = rl.Rectangle{ .x = 24, .y = 24 + 30 + 24, .width = 100, .height = 15 };
+    var iterations_f32: f32 = @floatFromInt(iterations.*);
+    _ = rg.slider(bounds, null, "Iterations", &iterations_f32, 1, 10);
+    iterations.* = @intFromFloat(iterations_f32);
+}
+
+fn kockSegment(p1: rl.Vector2, p2: rl.Vector2, iterations: usize, color: rl.Color) void {
+    // divide the line segment into thirds
+    const third_x = (p2.x - p1.x) / 3;
+    const third_y = (p2.y - p1.y) / 3;
+    const p1a = rl.Vector2{ .x = p1.x + third_x, .y = p1.y + third_y };
+    const p2a = rl.Vector2{ .x = p1.x + 2 * third_x, .y = p1.y + 2 * third_y };
+    // calculate the coordinates of the outward point of the triangle
+    // rotate the middle segment direction by -60 deg around p1a to get the apex
+    const p_mid = (rl.Vector2{ .x = p2a.x - p1a.x, .y = p2a.y - p1a.y }).rotate(-std.math.pi / 3.0).add(p1a);
+    // recursively draw the pattern on each of the four new segments
+    if (iterations > 0 and p1.distance(p1a) > 2) {
+        kockSegment(p1, p1a, iterations - 1, color);
+        kockSegment(p1a, p_mid, iterations - 1, color);
+        kockSegment(p_mid, p2a, iterations - 1, color);
+        kockSegment(p2a, p2, iterations - 1, color);
+    } else {
+        // draw the final line segments
+        rl.drawLineV(p1, p1a, color);
+        rl.drawLineV(p1a, p_mid, color);
+        rl.drawLineV(p_mid, p2a, color);
+        rl.drawLineV(p2a, p2, color);
+    }
+}
+
+fn kockSnowflake(origin_x: f32, origin_y: f32, size: f32) void {
+    const S = struct {
+        var iterations: usize = 5;
+    };
+    interationsSlider(&S.iterations);
+    // find the initial vertexes of the equilateral triangle
+    const color: rl.Color = .light_gray;
+    const height = size * 3 / 4;
+    const offset_y = size - height;
+    const p1 = rl.Vector2{ .x = origin_x, .y = origin_y + offset_y };
+    const p2 = p1.add(rl.Vector2{ .x = size, .y = 0 });
+    const p3 = p1.add(rl.Vector2{ .x = size / 2, .y = height });
+    // then recursively draw the snowflake pattern on each edge
+    kockSegment(p1, p2, S.iterations, color);
+    kockSegment(p2, p3, S.iterations, color);
+    kockSegment(p3, p1, S.iterations, color);
+}
+
 fn sierpinskiTriangleRecurse(p1: rl.Vector2, p2: rl.Vector2, p3: rl.Vector2, color: rl.Color, iterations: usize) void {
     if (iterations == 0) {
         rl.drawTriangle(p1, p2, p3, color);
@@ -40,6 +90,10 @@ fn sierpinskiTriangleRecurse(p1: rl.Vector2, p2: rl.Vector2, p3: rl.Vector2, col
 }
 
 fn sierpinskiTriangle(origin_x: f32, origin_y: f32, size: f32) void {
+    const S = struct {
+        var iterations: usize = 6;
+    };
+    interationsSlider(&S.iterations);
     // find the initial vertexes of the initial equilateral triangle
     const height = size * 3 / 4;
     const offset_y = (size - height) / 2;
@@ -47,50 +101,43 @@ fn sierpinskiTriangle(origin_x: f32, origin_y: f32, size: f32) void {
     const p2 = rl.Vector2{ .x = origin_x, .y = origin_y + offset_y + height };
     const p3 = p2.add(rl.Vector2{ .x = size, .y = 0 });
     // draw triangles recursively
-    const iterations = 6;
-    sierpinskiTriangleRecurse(p1, p2, p3, .light_gray, iterations);
+    sierpinskiTriangleRecurse(p1, p2, p3, .light_gray, S.iterations);
 }
 
-fn kockSegment(p1: rl.Vector2, p2: rl.Vector2, iterations: usize, color: rl.Color) void {
-    // divide the line segment into thirds
-    const third_x = (p2.x - p1.x) / 3;
-    const third_y = (p2.y - p1.y) / 3;
-    const p1a = rl.Vector2{ .x = p1.x + third_x, .y = p1.y + third_y };
-    const p2a = rl.Vector2{ .x = p1.x + 2 * third_x, .y = p1.y + 2 * third_y };
-    // calculate the coordinates of the outward point of the triangle
-    // rotate the middle segment direction by -60 deg around p1a to get the apex
-    const p_mid = (rl.Vector2{ .x = p2a.x - p1a.x, .y = p2a.y - p1a.y }).rotate(-std.math.pi / 3.0).add(p1a);
-    // recursively draw the pattern on each of the four new segments
+fn treeBranch(p1: rl.Vector2, p2: rl.Vector2, color: rl.Color, iterations: usize, angle: f32) void {
+    rl.drawLineV(p1, p2, color);
     if (iterations > 0) {
-        kockSegment(p1, p1a, iterations - 1, color);
-        kockSegment(p1a, p_mid, iterations - 1, color);
-        kockSegment(p_mid, p2a, iterations - 1, color);
-        kockSegment(p2a, p2, iterations - 1, color);
-    } else {
-        // draw the final line segments
-        rl.drawLineV(p1, p1a, color);
-        rl.drawLineV(p1a, p_mid, color);
-        rl.drawLineV(p_mid, p2a, color);
-        rl.drawLineV(p2a, p2, color);
+        // two new branches starting at the end of the previous branch
+        // 2/3 the length and rotated by 30 degrees in either direction
+        const p3 = p1.lerp(p2, 1.666);
+        const p4 = p3.subtract(p2).rotate(angle).add(p2);
+        const p5 = p3.subtract(p2).rotate(-angle).add(p2);
+        treeBranch(p2, p4, color, iterations - 1, angle);
+        treeBranch(p2, p5, color, iterations - 1, angle);
     }
 }
 
-fn kockSnowflake(origin_x: f32, origin_y: f32, size: f32) void {
-    // find the initial vertexes of the equilateral triangle
-    const color: rl.Color = .light_gray;
-    const height = size * 3 / 4;
-    const offset_y = size - height;
-    const p1 = rl.Vector2{ .x = origin_x, .y = origin_y + offset_y };
-    const p2 = p1.add(rl.Vector2{ .x = size, .y = 0 });
-    const p3 = p1.add(rl.Vector2{ .x = size / 2, .y = height });
-    // then recursively draw the snowflake pattern on each edge
-    const iterations = 5;
-    kockSegment(p1, p2, iterations, color);
-    kockSegment(p2, p3, iterations, color);
-    kockSegment(p3, p1, iterations, color);
+fn treeUi(iterations: *usize, angle: *f32) void {
+    // iterations slider
+    interationsSlider(iterations);
+    // angle slider
+    const bounds2 = rl.Rectangle{ .x = 24, .y = 24 + 30 + 24 + 15 + 15, .width = 100, .height = 15 };
+    _ = rg.slider(bounds2, null, "Angle", angle, 0, std.math.pi / 2.0);
 }
 
-const Fractal = enum(i32) { kockSnowflake, sierpinskiTriangle };
+fn tree(origin_x: f32, origin_y: f32, size: f32) void {
+    const S = struct {
+        var iterations: usize = 7;
+        var angle: f32 = std.math.pi / 6.0;
+    };
+    treeUi(&S.iterations, &S.angle);
+    const color: rl.Color = .light_gray;
+    const p1 = rl.Vector2{ .x = origin_x + size / 2, .y = origin_y + size };
+    const p2 = rl.Vector2{ .x = origin_x + size / 2, .y = origin_y + 2 * size / 3 };
+    treeBranch(p1, p2, color, S.iterations, S.angle);
+}
+
+const Fractal = enum(i32) { kockSnowflake, sierpinskiTriangle, tree };
 
 fn fractalSelectBtns(fractalType: *Fractal) void {
     const offset_x = 24 + 30 + 24;
@@ -99,7 +146,7 @@ fn fractalSelectBtns(fractalType: *Fractal) void {
     const size_y = 30;
     const r = rl.Rectangle{ .x = offset_x, .y = offset_y, .width = size_x, .height = size_y };
     var active = @intFromEnum(fractalType.*);
-    _ = rg.toggleGroup(r, "Kock Snowflake;Sierpinski Triangle", &active);
+    _ = rg.toggleGroup(r, "Kock Snowflake;Sierpinski Triangle;Tree", &active);
     fractalType.* = @enumFromInt(active);
 }
 
@@ -120,6 +167,7 @@ fn fractal() bool {
     switch (S.fractal) {
         .kockSnowflake => kockSnowflake(origin_x, origin_y, size),
         .sierpinskiTriangle => sierpinskiTriangle(origin_x, origin_y, size),
+        .tree => tree(origin_x, origin_y, size),
     }
     // draw back button
     if (backBtn()) {
